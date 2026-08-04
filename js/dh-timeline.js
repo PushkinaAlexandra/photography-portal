7de70838d4468348f4219e28d4b2352d22f77f9b
Alexandra Pushkina <pushkina.a.v.400@gmail.com>
2026-08-04 12:57:19 +0300

    Fix date in timeline

--- js/dh-timeline.js
+++ js/dh-timeline.js
@@ -6,20 +6,27 @@ fetch('../data/dh_photographers.json')
     .then(function(photographers) {
         // --- Prepare data for Vis Timeline ---
         var items = [];
+        var currentYear = new Date().getFullYear();
 
         // Add photographers as range items
         photographers.forEach(function(p) {
-            if (p.birthYear && p.deathYear) {
+            if (p.birthYear) {
+                // Use current year if photographer is still alive
+                var endYear = p.deathYear || currentYear;
+                var displayEnd = p.deathYear || 'present';
+
                 items.push({
                     id: p.id,
-                    content: p.name + '<br><small>' + p.birthYear + ' - ' + p.deathYear + '</small>',
-                    start: p.birthYear,
-                    end: p.deathYear,
+                    content: p.name + '<br><small>' + p.birthYear + ' - ' + displayEnd + '</small>',
+                    start: new Date(p.birthYear, 0, 1),
+                    end: new Date(endYear, 0, 1),
                     type: 'range',
-                    className: 'photographer'
+                    className: p.deathYear ? 'photographer' : 'photographer-alive'
                 });
             }
         });
+
+        console.log('Total photographers displayed:', items.length);
 
         // --- Initialize Timeline ---
         var container = document.getElementById('timeline');
@@ -33,23 +40,23 @@ fetch('../data/dh_photographers.json')
             stack: true,
             showCurrentTime: false,
             orientation: 'top',
-            height: '100%'
+            height: '100%',
+            min: new Date(1800, 0, 1),
+            max: new Date(2030, 0, 1),
+            zoomMin: 1000 * 60 * 60 * 24 * 365 * 5, // 5 years minimum zoom
+            zoomMax: 1000 * 60 * 60 * 24 * 365 * 200 // 200 years maximum zoom
         };
 
         // Create timeline instance
         var timeline = new vis.Timeline(container, items, options);
 
-        // --- Add legend ---
-        var legend = document.createElement('div');
-        legend.style.marginTop = '10px';
-        legend.style.color = '#b0a392';
-        legend.innerHTML = '<span style="display:inline-block;background:#4a6a8a;width:20px;height:20px;border-radius:4px;vertical-align:middle;"></span> Photographer lifespan';
-        container.parentNode.insertBefore(legend, container.nextSibling);
+        // Fit timeline to show all items
+        timeline.fit();
     })
     .catch(function(error) {
         console.error('Error loading data:', error);
         var timelineElement = document.getElementById('timeline');
         if (timelineElement) {
-            timelineElement.innerHTML = '<p style="color:red;">Error loading data.</p>';
-            }
+            timelineElement.innerHTML = '<p style="color:red;">Error loading data. Please check that dh_photographers.json exists.</p>';
+        }
     });
